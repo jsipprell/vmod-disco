b73f626cccfdc5e1fd7efe888a366321ca61d616
Jesse Sipprell <jsipprell@mcclatchyinteractive.com>
2016-06-05 04:26:36 -0400

    add 64bit atomics for 32-bit x86

--- include/atomic.h
+++ include/atomic.h
@@ -11,6 +11,7 @@ typedef struct { const void* volatile _atomic; } __attribute__((aligned(SIZEOF_V
 #define VATOMIC_INC32(vd,op) (vatomic_add32(&(op), 1)+1)
 #define VATOMIC_DEC32(vd,op) (vatomic_add32(&(op), -1)-1)
 
+#ifdef ARCH_X86_64
 static inline int64_t vatomic_add64(vatomic_uint64_t *v, int64_t i)
 {
   asm volatile("lock; xaddq %%rax, %2;"
@@ -19,7 +20,28 @@ static inline int64_t vatomic_add64(vatomic_uint64_t *v, int64_t i)
                :"memory");
   return i;
 }
+#elif defined(ARCH_X86)
+static inline int64_t vatomic_add64(vatomic_uint64_t *v, int64_t i)
+{
+  volatile uint32_t i32lo = i, i32hi = (i >> 32);
 
+  asm volatile("movl %%ebx, %%eax;"
+               "movl %%ecx, %%edx;"
+               "lock; cmpxchg8b %3;"
+               "1: movl %%eax, %%ebx;"
+               "movl %%edx, %%ecx;"
+               "addl %1, %%ebx;"
+               "adcl %2, %%ecx;"
+               "lock; cmpxchg8b %3;"
+               "jne 1b"
+               : "=&A" (i)
+               : "m" (i32lo), "m" (i32hi), "m" (v->_atomic)
+               :"ebx","ecx","memory");
+  return i;
+}
+#else
+# error "Unsupported architecture"
+#endif
 static inline int32_t vatomic_add32(vatomic_uint32_t *v, int32_t i)
 {
   asm volatile("lock; xaddl %%eax, %2;"
