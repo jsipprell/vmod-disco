e722d0d0634d5760fee4ade33bb02aac302f625f
Jesse Sipprell <jsipprell@mcclatchy.com>
2019-08-12 07:04:35 -0400

    make sure we don't indicate an update write lock if the underlying lock fails

--- src/update.c
+++ src/update.c
@@ -80,13 +80,24 @@ int update_rwlock_tryrdlock(struct update_rwlock *lock)
   return (pthread_rwlock_tryrdlock(&lock->lock));
 }
 
+/* Block and acquire a write lock if nobody else has one. Otherwise, try to acquire a read lock.
+ * wrlocked will be set to 1 if not NULL and the write lock was successful
+ * wrlocked will be set to 0 if not NULL and the write lock was already held by someone else.
+ * Returns: 0 upon either a successful read or write lock.
+ *          EBUSY write lock was held by someone else and read lock is busy.
+ *
+ * Function only blocks if a write lock is possible but read locks are pending.
+ */
 int update_rwlock_tryanylock(struct update_rwlock *lock, int *wrlocked)
 {
+  int ret;
   CHECK_OBJ_NOTNULL(lock, UPDATE_RWLOCK_MAGIC);
   if (VATOMIC_CAS32(lock->w, 0, 1)) {
-    if (wrlocked)
+    if ((ret = pthread_rwlock_wrlock(&lock->lock)) == 0 && wrlocked)
       *wrlocked = 1;
-    return pthread_rwlock_wrlock(&lock->lock);
+    else if (ret != 0)
+      AN(VATOMIC_CAS32(lock->w, 1, 0));
+    return ret;
   } else if(wrlocked)
     *wrlocked = 0;
   return pthread_rwlock_tryrdlock(&lock->lock);
